73469781ed2bd897b5cfe61150a82044db8e53b2
AgneBvdB <Agne.banzeryte@gmail.com>
2025-07-03 13:58:37 +0200

    Masonry connected

--- script.js
+++ script.js
@@ -3,150 +3,180 @@ const paintings = [
         src: 'assets/paintings/2016_WebP/AGUONĖLĖ. Pop., pastelė, 42x58 false.webp',
         title: 'AGUONĖLĖ 2016',
         description: 'Popierius, pastelė, 42x58 cm',
+        year: 2016
     },
     {
         src: 'assets/paintings/2016_WebP/Aguonėlės. Pop., pastelė, 45x58 false.webp',
         title: 'AGUONĖLĖS 2016',
         description: 'Popierius, pastelė, 45x58 cm',
+        year: 2016
     },
     {
         src: 'assets/paintings/2016_WebP/AGUONOS TAU. Pop., pastelė, 45x58.webp',
         title: 'AGUONOS TAU 2016',
         description: 'Popierius, pastelė, 45x58 cm',
+        year: 2016 
     },
         {
         src: 'assets/paintings/2016_WebP/Alyvos. Pop., pastelė 46x61 true.webp',
-        title: 'AGUONOS TAU 2016',
-        description: 'Popierius, pastelė, 45x58 cm',
+        title: 'ALYVOS 2016',
+        description: 'Popierius, pastelė, 46x61 cm',
+        year: 2016
     },
         {
         src: 'assets/paintings/2016_WebP/BERŽŲ AUKSAS. Pop., pastelė, 64x46.webp',
-        title: 'AGUONOS TAU 2016',
-        description: 'Popierius, pastelė, 45x58 cm',
+        title: 'BERŽŲ AUKSAS 2016',
+        description: 'Popierius, pastelė, 64x46 cm',
+        year: 2016
     },
         {
         src: 'assets/paintings/2016_WebP/IEVOS II,pop., pastelė, 36x50.webp',
-        title: 'AGUONOS TAU 2016',
-        description: 'Popierius, pastelė, 45x58 cm',
+        title: 'IEVOS II 2016',
+        description: 'Popierius, pastelė, 36x50 cm',
+        year: 2016
     },
         {
         src: 'assets/paintings/2016_WebP/IEVOS. pop., pastelė, 44x50 true.webp',
-        title: 'AGUONOS TAU 2016',
-        description: 'Popierius, pastelė, 45x58 cm',
+        title: 'IEVOS 2016',
+        description: 'Popierius, pastelė, 44x50 cm',
+        year: 2016
     },
         {
         src: 'assets/paintings/2016_WebP/Iš Audronės gėlynų.Šalavijai. Pop., pastelė, 36x50 true.webp',
-        title: 'AGUONOS TAU 2016',
-        description: 'Popierius, pastelė, 45x58 cm',
+        title: 'ŠALAVIJAI 2016',
+        description: 'Popierius, pastelė, 36x50 cm',
+        year: 2016
     },
         {
         src: 'assets/paintings/2016_WebP/KIAULPIENĖS, pop., pastelė 50x37 true.webp',
-        title: 'AGUONOS TAU 2016',
-        description: 'Popierius, pastelė, 45x58 cm',
+        title: 'KIAULPIENĖS 2016',
+        description: 'Popierius, pastelė, 50x37 cm',
+        year: 2016
     },
         {
         src: 'assets/paintings/2016_WebP/Klevai. Pop., pastelė, 46x66 false.webp',
-        title: 'AGUONOS TAU 2016',
-        description: 'Popierius, pastelė, 45x58 cm',
+        title: 'KLEVAI 2016',
+        description: 'Popierius, pastelė, 46x66 cm',
+        year: 2016
     },
         {
         src: 'assets/paintings/2016_WebP/Mėlynos pušys. Pop., pastelė 42X55-false.webp',
-        title: 'AGUONOS TAU 2016',
-        description: 'Popierius, pastelė, 45x58 cm',
+        title: 'MĖLYNOS PUŠYS 2016',
+        description: 'Popierius, pastelė, 42x55 cm',
+        year: 2016
     },
         {
         src: 'assets/paintings/2016_WebP/PAVASARIS KOPOSE, pop., pastelė, 42X62-false.webp',
-        title: 'AGUONOS TAU 2016',
-        description: 'Popierius, pastelė, 45x58 cm',
+        title: '/PAVASARIS KOPOSE 2016',
+        description: 'Popierius, pastelė, 42x62 cm',
+        year: 2016
     },
         {
         src: 'assets/paintings/2016_WebP/PO DU. pop., akvarelė 45x60-false.webp',
-        title: 'AGUONOS TAU 2016',
-        description: 'Popierius, pastelė, 45x58 cm',
+        title: 'PO DU 2016',
+        description: 'Popierius, akvarelė, 45x60 cm',
+        year: 2016
     },
         {
         src: 'assets/paintings/2016_WebP/RAMUNĖS II. Pastelė false.webp',
-        title: 'AGUONOS TAU 2016',
-        description: 'Popierius, pastelė, 45x58 cm',
+        title: 'RAMUNĖS II 2016',
+        description: 'Popierius, pastelė',
+        year: 2016
     },
         {
         src: 'assets/paintings/2016_WebP/RAMUNĖS, kartonas, pastelė,45x58 cm false.webp',
-        title: 'AGUONOS TAU 2016',
-        description: 'Popierius, pastelė, 45x58 cm',
+        title: 'RAMUNĖS 2016',
+        description: 'Kartonas, pastelė, 45x58 cm',
+        year: 2016
     },
         {
-        src: 'assets/paintings/2016_WebP/AGUONOS TAU. Pop., pastelė, 45x58.webp',
-        title: 'AGUONOS TAU 2016',
-        description: 'Popierius, pastelė, 45x58 cm',
+        src: 'assets/paintings/2016_WebP/Rudeninė nuotaika, pop., pastelė 50x38.webp',
+        title: 'RUDENINĖ NUOTAIKA 2016',
+        description: 'Popierius, pastelė, 50x38 cm',
+        year: 2016
     },
         {
-        src: 'assets/paintings/2016_WebP/AGUONOS TAU. Pop., pastelė, 45x58.webp',
-        title: 'AGUONOS TAU 2016',
-        description: 'Popierius, pastelė, 45x58 cm',
+        src: 'assets/paintings/2016_WebP/RŪKAS SKLAIDOSI. Pop., pastelė, 42x62 false.webp',
+        title: 'RŪKAS SKLAIDOSI 2016',
+        description: 'Popierius, pastelė, 42x62 cm',
+        year: 2016
     },
         {
-        src: 'assets/paintings/2016_WebP/AGUONOS TAU. Pop., pastelė, 45x58.webp',
-        title: 'AGUONOS TAU 2016',
-        description: 'Popierius, pastelė, 45x58 cm',
+        src: 'assets/paintings/2016_WebP/Takas iš pušyno. Pop., pastelė.webp',
+        title: 'TAKAS IŠ PUŠYNO 2016',
+        description: 'Popierius, pastelė',
+        year: 2016
     },
         {
-        src: 'assets/paintings/2016_WebP/AGUONOS TAU. Pop., pastelė, 45x58.webp',
-        title: 'AGUONOS TAU 2016',
-        description: 'Popierius, pastelė, 45x58 cm',
+        src: 'assets/paintings/2016_WebP/TAS SALDUS ALYVŲ KVAPAS. Pop., pastelė, 42x62 true.webp',
+        title: 'TAS SALDUS ALYVŲ KVAPAS 2016',
+        description: 'Popierius, pastelė, 42x62 cm',
+        year: 2016
     },
         {
-        src: 'assets/paintings/2016_WebP/AGUONOS TAU. Pop., pastelė, 45x58.webp',
-        title: 'AGUONOS TAU 2016',
-        description: 'Popierius, pastelė, 45x58 cm',
+        src: 'assets/paintings/2016_WebP/TIK NENUPŪSK. Pop., pastelė, 46x61 false.webp',
+        title: 'TIK NENUPŪSK 2016',
+        description: 'Popierius, pastelė, 46x61 cm',
+        year: 2016
     },
         {
-        src: 'assets/paintings/2016_WebP/AGUONOS TAU. Pop., pastelė, 45x58.webp',
-        title: 'AGUONOS TAU 2016',
-        description: 'Popierius, pastelė, 45x58 cm',
+        src: 'assets/paintings/2016_WebP/untitled-2.webp',
+        title: 'BE PAVADINIMO 2016',
+        description: 'Popierius, pastelė',
+        year: 2016
     },
         {
-        src: 'assets/paintings/2016_WebP/AGUONOS TAU. Pop., pastelė, 45x58.webp',
-        title: 'AGUONOS TAU 2016',
-        description: 'Popierius, pastelė, 45x58 cm',
+        src: 'assets/paintings/2016_WebP/untitles.webp',
+        title: 'BE PAVADINIMO 2016',
+        description: 'Popierius, pastelė',
+        year: 2016
     },
         {
-        src: 'assets/paintings/2016_WebP/AGUONOS TAU. Pop., pastelė, 45x58.webp',
-        title: 'AGUONOS TAU 2016',
-        description: 'Popierius, pastelė, 45x58 cm',
+        src: 'assets/paintings/2016_WebP/VĖJAS KOPOSE, pop., pastelė, 46x66 true.webp',
+        title: 'VĖJAS KOPOSE 2016',
+        description: 'Popierius, pastelė, 46x66 cm',
+        year: 2016
     },
         {
-        src: 'assets/paintings/2016_WebP/AGUONOS TAU. Pop., pastelė, 45x58.webp',
-        title: 'AGUONOS TAU 2016',
-        description: 'Popierius, pastelė, 45x58 cm',
+        src: 'assets/paintings/2016_WebP/Viendienės. Pop., pastelė, 36x50 false.webp',
+        title: 'VIENADIENĖS 2016',
+        description: 'Popierius, pastelė, 36x50 cm',
+        year: 2016
     },
         {
-        src: 'assets/paintings/2016_WebP/AGUONOS TAU. Pop., pastelė, 45x58.webp',
-        title: 'AGUONOS TAU 2016',
-        description: 'Popierius, pastelė, 45x58 cm',
+        src: 'assets/paintings/2016_WebP/Visiška impresija. Pop., pastelė, 46x66 ture.webp',
+        title: 'VISIŠKA IMPRESIJA 2016',
+        description: 'Popierius, pastelė, 46x66 cm',
+        year: 2016
     },
         {
-        src: 'assets/paintings/2016_WebP/AGUONOS TAU. Pop., pastelė, 45x58.webp',
-        title: 'AGUONOS TAU 2016',
-        description: 'Popierius, pastelė, 45x58 cm',
+        src: 'assets/paintings/2016_WebP/VYŠNIA ŽYDI, pop.,pastelė 42x62-true.webp',
+        title: 'VYŠNIA ŽYDI 2016',
+        description: 'Popierius, pastelė, 42x62 cm',
+        year: 2016
     },
         {
-        src: 'assets/paintings/2016_WebP/AGUONOS TAU. Pop., pastelė, 45x58.webp',
-        title: 'AGUONOS TAU 2016',
-        description: 'Popierius, pastelė, 45x58 cm',
+        src: 'assets/paintings/2016_WebP/VYŠNIA, pop., pastelė, 38x51 true.webp',
+        title: 'VYŠNIA 2016',
+        description: 'Popierius, pastelė, 38x51 cm',
+        year: 2016
     },
         {
-        src: 'assets/paintings/2016_WebP/AGUONOS TAU. Pop., pastelė, 45x58.webp',
-        title: 'AGUONOS TAU 2016',
-        description: 'Popierius, pastelė, 45x58 cm',
+        src: 'assets/paintings/2016_WebP/ŽOLYNAI II. Pop., pastelė, 62x44 false.webp',
+        title: 'ŽOLYNAI II 2016',
+        description: 'Popierius, pastelė, 62x44 cm',
+        year: 2016
     },
         {
-        src: 'assets/paintings/2016_WebP/AGUONOS TAU. Pop., pastelė, 45x58.webp',
-        title: 'AGUONOS TAU 2016',
-        description: 'Popierius, pastelė, 45x58 cm',
+        src: 'assets/paintings/2016_WebP/ŽOLYNAI. Pop., pastelė, 62x42 true.webp',
+        title: 'ŽOLYNAI 2016',
+        description: 'Popierius, pastelė, 62x42 cm',
+        year: 2016
     },
         {
-        src: 'assets/paintings/2016_WebP/AGUONOS TAU. Pop., pastelė, 45x58.webp',
-        title: 'AGUONOS TAU 2016',
-        description: 'Popierius, pastelė, 45x58 cm',
+        src: 'assets/paintings/2016_WebP/ŽYDINTI, pop.,akvarelė 45X60 false.webp',
+        title: 'ŽYDINTI 2016',
+        description: 'Popierius, akvarelė, 45X60 cm',
+        year: 2016
     },
 ]
